6bcdafa752c728e7d64916b00f4d05144439c481
Cafe Bit <43447794+cafeibit@users.noreply.github.com>
2022-04-30 17:16:33 -0400

    Update and rename DataEngineeringByPython.ipynb to DataEngineeringByPython.py

---
+++ DataEngineeringByPython.py
@@ -0,0 +1,9 @@
+# MAGIC %md
+# MAGIC # Data Processing in Python
+# MAGIC #### Data preprocessing is the first step in data visualization, data analytics, and machine learning, where data is prepared for analytics functions to get the best possible insights. 
+
+# MAGIC * To use different technical and analytical aspects of data preprocessing – data collection, data cleaning, data integration, data reduction, and data transformation and get to grips with implementing them using the open source Python programming environment, lead to more effective decision making. 
+# MAGIC * To use Python to read, manipulate, and analyze data; perform data cleaning, integration, reduction, and transformation techniques; and handle outliers or missing values to effectively prepare data for analytic tools.
+
+# MAGIC ### Data Clean
+
